ec46e458c45071f88c106db4e469a1b382bbd23e
Ankit Mishra <73629134+theankitmishra77@users.noreply.github.com>
2024-12-25 15:21:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -327,9 +327,9 @@ def extract_pdf():
         response['header']['PDF_NAME'] = data['PDF_NAME']
         response['header']['MSG_STATUS'] = 'S'
         response['header']['ERROR_MSG'] = ''
-        if response['header']["IGST_TAX_AMOUNT"] != 0 or response['header']["IGST_TAX_AMOUNT"] != '':
+        if response['header']["IGST_TAX_AMOUNT"] != 0:
             response['items']['IGST_RATE'] = int((response['header']["IGST_TAX_AMOUNT"]*100)/(response['header']["TOTAL_INVOICE_AMOUNT"]-response['header']["IGST_TAX_AMOUNT"]))
-        elif response['header']["CGST_TAX_AMOUNT"] != 0 or response['header']["CGST_TAX_AMOUNT"] != '':
+        elif response['header']["CGST_TAX_AMOUNT"] != 0:
             response['items']["CGST_RATE"] = int((response['header']["CGST_TAX_AMOUNT"]*100)/(response['header']["TOTAL_INVOICE_AMOUNT"]-response['header']["CGST_TAX_AMOUNT"]))
             response['items']["SGST_RATE"] = int((response['header']["CGST_TAX_AMOUNT"]*100)/(response['header']["TOTAL_INVOICE_AMOUNT"]-response['header']["CGST_TAX_AMOUNT"]))
         print('Final Response',response)
